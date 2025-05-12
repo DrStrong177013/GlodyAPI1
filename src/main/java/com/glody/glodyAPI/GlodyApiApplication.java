1547e0afb5740124c9d964bb6aed7d5d6ae7c18d
DrStrong177013 <manhtdse173612@fpt.edu.vn>
2025-05-12 15:14:32 +0700

    first commit

---
+++ src/main/java/com/glody/glodyAPI/GlodyApiApplication.java
@@ -0,0 +1,13 @@
+package com.glody.glodyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlodyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlodyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glody/glodyAPI/GlodyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glody.glodyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlodyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
